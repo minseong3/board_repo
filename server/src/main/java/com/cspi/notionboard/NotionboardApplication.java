fde0baccbbb90eed6f09c565b44fe43a4612408e
minseong3 <alstjd1021@naver.com>
2025-02-10 10:10:20 +0900

    enviroment config

---
+++ server/src/main/java/com/cspi/notionboard/NotionboardApplication.java
@@ -0,0 +1,13 @@
+package com.cspi.notionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionboardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/cspi/notionboard/NotionboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cspi.notionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
